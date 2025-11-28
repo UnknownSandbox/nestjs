fda0fafa501f43c856cffeda098caa6c2d7893f8
unknownhero <unknownpostie@gmail.com>
2025-11-28 17:33:27 +0300

    feat(tools): add swc and vitest

---
+++ nest-docs/example-01/vitest.config.e2e.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path';
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  resolve: {
+    alias: {
+      '@src': resolve(__dirname, './src'),
+      '@test': resolve(__dirname, './test'),
+    },
+  },
+  plugins: [swc.vite({ module: { type: 'es6' } })],
+});
