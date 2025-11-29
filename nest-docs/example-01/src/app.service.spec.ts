fde3f18e2616b6f7744a4103ff0cdd1b9fadf550
unknownhero <unknownpostie@gmail.com>
2025-11-29 19:15:57 +0300

    feat(test): add unit and e2e tests with vitest

--- nest-docs/example-01/src/app.service.spec.ts
+++ nest-docs/example-01/src/app.service.spec.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import {beforeEach, describe, expect, it } from 'vitest';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -12,12 +12,12 @@ describe('AppController', () => {
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appService.getHello()).toBe('Hello World!');
     });
   });
 });
--- nest-docs/example-01/vitest.config.e2e.ts
+++ nest-docs/example-01/vitest.config.e2e.ts
@@ -1,17 +1,21 @@
-import { resolve } from 'path';
-import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
+import swc from 'unplugin-swc';
 
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
+    exclude: [
+      'dist/**',
+      '**/dist/**',
+      'node_modules/**'
+    ],
     globals: true,
-    root: './',
-  },
-  resolve: {
-    alias: {
-      '@src': resolve(__dirname, './src'),
-      '@test': resolve(__dirname, './test'),
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+      reportsDirectory: './coverage/e2e',
+      reporter: ['text', 'html', 'lcov'],
+      exclude: ['dist/**', '**/dist/**']
     },
   },
   plugins: [swc.vite({ module: { type: 'es6' } })],
